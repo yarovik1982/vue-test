f0636dcd95b44abd6d2b019d81a4494278eae1e5
acunamatata82 <yar.oleg1982@mail.ru>
2023-06-22 18:44:08 +0300

    commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="app">
+    <h1>Редактирование структуры данных</h1>
+    <data-list :data="data" @save="saveData" />
+  </div>
+</template>
+
+<script>
+import DataList from "@/components/DataList.vue";
+
+export default {
+  components: {
+    DataList,
+  },
+  data() {
+    return {
+      data: [
+        { name: "foo1", value: "bar1" },
+        { name: "foo2", value: "bar2" },
+        { name: "foo3", value: "bar3" },
+        { name: "foo4", value: "bar4" },
+        { name: "foo5", value: "bar5" },
+      ],
+    };
+  },
+  methods: {
+    saveData(index, newValue) {
+      this.data[index].value = newValue;
+    },
+  },
+};
+</script>
+
+<style>
+.app {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+  font-family: Arial, sans-serif;
+}
+
+h1 {
+  margin-bottom: 20px;
+}
+</style>
